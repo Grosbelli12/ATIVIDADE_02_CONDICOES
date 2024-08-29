51e80dbb73793ffdece75d187f94048432b1c1bb
Grosbelli112 <gustavogrosbelli11@gmail.com>
2024-08-29 08:29:49 -0300

    atividae 7 com switch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner md = new Scanner(System.in);
+        System.out.println("insira a média final do aluno ");
+      int mdf = md.nextInt();
+if (mdf >= 7) {
+    System.out.println(": aprovado");
+}
+else if ( mdf >= 5 && mdf < 7 ) {
+    System.out.println(": Recuperação");
+}else {
+    System.out.println(": reprovado");
+        }
+
+
+
+
+
     }
 }

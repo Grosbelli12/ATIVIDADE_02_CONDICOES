a925bedba2b884abe5ec4af1b8fa4e325dda76f4
GustavoGrosbelli <gustavogrosbelli26@gmail.com>
2024-09-02 10:39:51 -0300

    nome change

--- src/atividade_6_verificação_de_Aprovação_Escolar.java
+++ src/atividade_6_verificação_de_Aprovação_Escolar.java
@@ -1,35 +1,22 @@
 import java.util.Scanner;
 
-public class atividade_6_verificação_de_Aprovação_Escolar {
+public class atividade_6_verificação_de_Aprovação_Escolar{
     public static void main(String[] args) {
-        Scanner cl = new Scanner(System.in);
-
-        System.out.println("Digite o primeiro numero: ");
-        int n1 = cl.nextInt();
-
-        System.out.println("Digite o segundo numero: ");
-        int n2 = cl.nextInt();
-
-        System.out.println("Digite 1 se deseja somar, 2 para subtrair, 3 se deseja multiplicar e 4 se deseja dividir 1: ");
-        int op = cl.nextInt();
-
-        switch (op) {
-            case 1:
-                System.out.println(n1 + n2);
-                break;
-            case 2:
-                System.out.println(n1 - n2);
-                break;
-            case 3:
-                System.out.println(n1 * n2);
-                break;
-            case 4:
-                System.out.println(n1 / n2);
-                break;
+        Scanner md = new Scanner(System.in);
+        System.out.println("insira a média final do aluno ");
+      int mdf = md.nextInt();
+if (mdf >= 7) {
+    System.out.println(": aprovado");
+}
+else if ( mdf >= 5 && mdf < 7 ) {
+    System.out.println(": Recuperação");
+}else {
+    System.out.println(": reprovado");
         }
 
+
+
+
+
     }
-
-
 }
-
